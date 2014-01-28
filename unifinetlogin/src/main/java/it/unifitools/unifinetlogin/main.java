2f0c3c1d17c59cb4e47e5d498494226b9d649449
giuse <demon.2347@gmail.com>
2014-01-28 15:46:10 +0100

    Sistemata la gestione del thread

--- unifinetlogin/src/main/java/it/unifitools/unifinetlogin/main.java
+++ unifinetlogin/src/main/java/it/unifitools/unifinetlogin/main.java
@@ -11,20 +11,40 @@ import android.util.Log;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.content.Intent;
 
 
 public class main extends Activity {
     public static String appTag = "unl";
     public static String prefs = "unlPrefs";
     private String matr,pwd;
-    private static SharedPreferences settings;
+    private SharedPreferences settings;
+    private Worker work;
+    private Button btn;
+    private EditText m,p;
+    private String sta, sto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(appTag,"Applicazione partita");
+        btn = (Button) findViewById(R.id.btn);
+        sta = getResources().getString(R.string.start);
+        sto = getResources().getString(R.string.stop);
+        try{
+            if(work.isAlive()){
+                btn.setText(sta);
+            }
+            Log.i(appTag,"secondo Avvio");
+        }catch(NullPointerException e){
+            Log.i(appTag,"Primo Avvio");
+        }
+
 
         setContentView(R.layout.activity_main);
+
+        m = (EditText) findViewById(R.id.matri);
+        p = (EditText) findViewById(R.id.passi);
 
         settings = getSharedPreferences(prefs, 0);
         matr = settings.getString("matricola","");
@@ -48,9 +68,10 @@ public class main extends Activity {
     }
 
     public boolean check(){
-        EditText m = (EditText) findViewById(R.id.matri);
-        EditText p = (EditText) findViewById(R.id.passi);
+
         if(!m.getText().equals("") && !p.getText().equals("")){
+            matr = m.getText().toString();
+            pwd = p.getText().toString();
             return true;
         }
 
@@ -60,20 +81,22 @@ public class main extends Activity {
 
     public void toggle(View v){
         Log.i(appTag,"Bottone premuto");
-        Button btn = (Button) findViewById(R.id.btn);
-        String sta = getResources().getString(R.string.start);
-        String sto = getResources().getString(R.string.stop);
+        btn = (Button) findViewById(R.id.btn);
         if(btn.getText().equals(sta)){
             if(!check()){
                 return;
             }
             save();
-            if(!checkConn()){
+           /* if(!checkConn()){
                 return;
-            }
+            }*/
+
+            work = new Worker(matr, pwd);
+            work.start();
             btn.setText(sto);
-            save();
         }else{
+            work.stopThread();
+            work = null;
             btn.setText(sta);
         }
     }
@@ -88,16 +111,23 @@ public class main extends Activity {
         editor.putString("matricola",matr);
         editor.putString("password",pwd);
         editor.commit();
+        editor.apply();
+    }
+
+    @Override
+    public void onBackPressed(){
+        try{
+            if(work.isAlive()){
+                work.stopThread();
+            }
+        }catch(NullPointerException e){}
+        finish();
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-
         Log.i(appTag,"Applicazione terminata.");
-
-
-        finish();
     }
 
 }
